f539561c6fe7935ee174248798492cb7fea5bc8d
tto658 <114577624+tto658@users.noreply.github.com>
2025-02-19 11:37:53 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("nav a").forEach(anchor => {
+        anchor.addEventListener("click", function (event) {
+            event.preventDefault();
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+            window.scrollTo({
+                top: targetElement.offsetTop - 50,
+                behavior: "smooth"
+            });
+        });
+    });
+});
 
